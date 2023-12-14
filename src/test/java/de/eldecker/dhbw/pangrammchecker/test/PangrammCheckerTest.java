c549d209cfcd8709ff0b32515244c9eb287f97d4
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-14 09:24:59 +0100

    Erste Unit-Tests laufen gruen

---
+++ src/test/java/de/eldecker/dhbw/pangrammchecker/test/PangrammCheckerTest.java
@@ -0,0 +1,15 @@
+package de.eldecker.dhbw.pangrammchecker.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PangrammCheckerTest  {
+
+    @Test
+    public void testApp() {
+
+        assertTrue(2 > 1);
+    }
+
+}
