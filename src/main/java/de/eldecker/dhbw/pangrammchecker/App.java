a3f548edf52a9589ba26b71654bb55bccb836f38
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-14 09:00:50 +0100

    Maven-Projekt aufgesetzt, JUnit auf Jupiter aktualisiert

---
+++ src/main/java/de/eldecker/dhbw/pangrammchecker/App.java
@@ -0,0 +1,10 @@
+package de.eldecker.dhbw.pangrammchecker;
+
+public class App  {
+    
+    public static void main( String[] args ) {
+        
+        System.out.println( "Hello World!" );
+    }
+    
+}
